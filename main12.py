3f13d851632c2b7c471967909507ac8c21dae4cf
r0me777 <58448938+r0me777@users.noreply.github.com>
2021-11-14 22:19:28 -0600

    Add files via upload

---
+++ main12.py
@@ -0,0 +1,42 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.metrics import dp
+from kivy.core.audio import SoundLoader
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class AddDeleteShuffle(BoxLayout):
+    pass
+
+class PlayList(BoxLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        for i in range(0,10):
+            size = dp(100)
+            b = Button(text=str(i+1), size_hint=(1,None), size=(size,size))
+            self.add_widget(b)
+
+class PausePlay(BoxLayout):
+    pass
+
+class SongText(BoxLayout):
+    pass
+
+class AppLayout(BoxLayout):
+    pass
+
+class Sound5App(App):
+    def build(self):
+        return AppLayout()
+
+
+
+if __name__ == "__main__":
+    Sound5App().run()
